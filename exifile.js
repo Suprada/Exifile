ff6afcf2718a1c8359fff1c70c828d4a7cdecee3
Suprada Urval <suprada@gmail.com>
2019-03-29 14:16:54 -0700

    update emails everywhere

--- exifile.js
+++ exifile.js
@@ -4,7 +4,7 @@
 // TODO clean up code
 /* Exifile v3.0 - Free your Scribd highlights from the cloud
  * http://www.oddumbrella.com/exifile
- * Copyright (c) 2016 Suprada Urval <suprada@suprada.com> (http://www.suprada.com)
+ * Copyright (c) 2016 Suprada Urval <suprada@.oddumbrella.com> (https://oddumbrella.com)
  * MIT Licensed*/
 
 exifile = (function() {
@@ -15,11 +15,11 @@ exifile = (function() {
     description:
       "When you create highlights and notes when reading your Scribd books, there is no way to see them in one page or to download them. This bookmarklet helps you to excise and file those Scribd highlights.  It gives you a single clean page with all your notes and highlights which can be copies, and gives you the option to download it in plain text format or JSON format. The highlights and notes can also be downloaded in plain text or JSON format.",
     keywords: ["scribd", "note", "highlight"],
-    author: "Suprada Urval <suprada@suprada.com> (http://www.suprada.com)",
+    author: "Suprada Urval <suprada@oddumbrella.com> (https://oddumbrella.com)",
     bugs: {
-      email: "suprada@suprada.com"
+      email: "suprada@oddumbrella.com"
     },
-    homepage: "http://www.oddumbrella.com/exifile",
+    homepage: "https://oddumbrella.com/exifile",
     license: "MIT"
   };
 
